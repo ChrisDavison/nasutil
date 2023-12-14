82aa94a65d9c7503aa69c913e08e8a42eb15e6c5
Chris Davison <c.jr.davison@gmail.com>
2023-12-14 15:46:39 +0000

    wip filter output of python version

--- nasutil.py
+++ nasutil.py
@@ -3,47 +3,57 @@ from argparse import ArgumentParser
 from pathlib import Path
 import subprocess
 import os
-import sys
+import re
+
 
 def dl_file():
     fn = os.environ["NASUTIL_FILE"]
     return Path(fn).expanduser()
 
+
 def outdir():
-    fn = os.environ["NASUTIL_DIR"]
+    return os.environ["NASUTIL_DIR"]
+
 
 def get_current_urls():
     return set(dl_file().read_text().splitlines())
 
+
 def next_url_from_file():
     return next(thing for thing in get_current_urls())
+
 
 def add_url(url):
     current = get_current_urls()
     if not url:
         try:
             import pyperclip
+
             url = pyperclip.paste()
         except ImportError:
-            url = input('URL: ')
-    url = next(url.split('&'))
+            url = input("URL: ")
+    url = next(url.split("&"))
     current.add(url)
-    dl_file.write_text('\n'.join(current))
+    dl_file.write_text("\n".join(current))
+
 
 def remove_from_file(url):
     current = get_current_urls()
     current.remove(url)
-    dl_file.write_text('\n'.join(current))
+    dl_file.write_text("\n".join(current))
+
 
 def list():
     for line in get_current_urls():
         print(line)
+
 
 def download():
     while current := next_url_from_file():
         download_url(current)
         remove_from_file(current)
         break
+
 
 def download_url(url):
     command = [
@@ -59,7 +69,15 @@ def download_url(url):
         "%(uploader)s---%(title)s.%(ext)s",
         "--restrict-filenames",
     ]
-    proc = subprocess.run(command, cwd=outdir(), stdout=sys.stdout, stderr=sys.stderr)
+    proc = subprocess.Popen(command, cwd=outdir(), stdout=subprocess.PIPE)
+    rx_title = re.compile(r"[download] Destination: (.*)")
+    for line in proc.stdout:
+        line = line.decode().strip()
+        if m := rx_title.search(line):
+            print(m)
+            print(">>>", m)
+            break
+
 
 def clear_downloads():
     fn = os.environ["NASUTIL_FILE"]
@@ -70,12 +88,12 @@ def clear_downloads():
 if __name__ == "__main__":
     parser = ArgumentParser()
     cmds = parser.add_subparsers(dest="command")
-    cmds.add_parser('list', help="list videos to download", aliases=["ls", "l"])
-    add = cmds.add_parser('add', help="add a video", aliases=["a"])
-    cmds.add_parser('download', aliases=["d", "dl"])
-    cmds.add_parser('version', aliases=["v"])
-    cmds.add_parser('empty', aliases=["e", "clear"])
-    add.add_argument('video', type=str)
+    cmds.add_parser("list", help="list videos to download", aliases=["ls", "l"])
+    add = cmds.add_parser("add", help="add a video", aliases=["a"])
+    cmds.add_parser("download", aliases=["d", "dl"])
+    cmds.add_parser("version", aliases=["v"])
+    cmds.add_parser("empty", aliases=["e", "clear"])
+    add.add_argument("video", type=str)
 
     args = parser.parse_args()
 
